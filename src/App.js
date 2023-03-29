8a97a60412e9bd6cb5e8b0539b9aee61c4eeb9ef
Brian Mugo <brayomugo2015@gmail.com>
2023-03-29 11:36:07 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';  // Used to make GET requests from APIs, install it: npm install --save axios
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class Component
+class App extends React.Component {
+    // State is like a global object containing all most important things in that component
+    state = { advice: '' };
+
+    // Part of Component lifecycle. Executes exactly at the render of our component
+    componentDidMount() {
+        // console.log("Component Did MOUNT");
+        this.fetchAdvice();
+        
+    }
+
+    // method to fetch 
+    fetchAdvice = () =>   {
+        axios.get('https://api.adviceslip.com/advice')
+        .then((response) => {
+            const { advice } = response.data.slip;
+            // console.log(advice);
+            
+            this.setState({advice});
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+
+    render() {
+        const {advice} = this.state;
+
+        return (
+            <div className='app'>
+                <div className='card'>
+                    <h1 className='heading'>{advice}</h1>
+                    <button className='button' onClick={this.fetchAdvice}>
+                        <span>GIVE ME ADVICE!</span>
+                    </button>
+                </div>
+            </div>
+        );
+    }
 }
 
+// responsible for imorting class App to other files
 export default App;
